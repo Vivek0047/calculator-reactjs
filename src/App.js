306d46379dfd8156d935c48ef3899015f486a87a
Vivek Kumar <vkumar@greenapplesolutions.com>
2021-08-11 23:53:50 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import SimpleCalculator from './components/SimpleCalculator';
+import './main.css'
 
 function App() {
   return (
--- src/components/Button.js
+++ src/components/Button.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Button({ dispatch, text }) {
     return (
         <React.Fragment>
-            <button onClick={dispatch}>{text}</button>
+            <button className="button" onClick={dispatch}>{text}</button>
         </React.Fragment>
     )
 }
